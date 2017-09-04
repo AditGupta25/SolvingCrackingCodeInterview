fbd0bf7545a4f73938099ba28ddc73eb7d0492f5
Adit <aditgupta1@yahoo.com>
2017-09-03 21:52:07 -0400

    Chapter 1 and question one

---
+++ Chapter 1/UniqueString.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.*;
+
+
+public class UniqueString{
+	public static void main(String[] args) {
+		System.out.println("Welcome to Unique Character Checker. Please input a string:");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        System.out.println(input);
+	}
+}
