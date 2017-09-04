d4c7d6df8e7eef32a51b89761db12ec4d2147e64
Adit <aditgupta1@yahoo.com>
2017-09-04 19:05:04 -0400

    URLify

---
+++ Chapter 1/1-3/URLify.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.*;
+import java.lang.Iterable;
+
+//In this program I need to implement an algorithm to determine if 2 strings are permutations of each other
+public class URLify{
+
+	public static void main(String[] args) {
+
+		System.out.println("");
+		Scanner scan = new Scanner(System.in);
+		System.out.println("This program is meant to turn your ordinary string input into a URL output");
+		System.out.println("For example: \"john is cool\" will turn into \"john%20is%20cool\"");
+		System.out.println("Please input your string:");
+        String input1 = scan.nextLine();
+
+
+	}
+	
+}
+
